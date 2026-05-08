2427bbd353e91b38bd63d8fcea0d4effedc0e5c6
anton krotov <twinkidr@gmail.com>
2026-05-08 22:07:09 +0800

    add new constructor for graph

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -1,4 +1,6 @@
 #include"graph.hpp"
+#include<fstream>
+#include<sstream>
 
 Graph::~Graph() {
     for (Node* node : nodes) delete node;
@@ -22,4 +24,36 @@ Node* Graph::find_node(const std::string& name) const {
         if (node->get_name() == name) return node;
     }
     return nullptr;
+}
+
+Graph::Graph(const std::string &file_name) {
+    using namespace std;
+    ifstream file(file_name);
+    std::string line;
+
+    if (!file.is_open()) return;
+
+    getline(file, line);
+
+    while (getline(file, line)) {
+        if (line.empty()) continue;
+
+        stringstream ss(line);
+        string u_name, v_name;
+
+
+        if (ss >> u_name >> v_name) {
+            Node* u = find_node(u_name);
+            if (u == nullptr) {
+                u = new Node(u_name);
+                add_node(u);
+            }
+            Node* v = find_node(v_name);
+            if (v == nullptr) {
+                v = new Node(v_name);
+                add_node(v);
+            }
+            add_edge(u, v);
+        }
+    }
 }
--- graph/graph.hpp
+++ graph/graph.hpp
@@ -6,6 +6,7 @@ class Graph {
     Node* find_node(const std::string& name) const;
 public:
 
+    Graph(const std::string& file_name);
     ~Graph();
 
     void add_node(Node* node) {nodes.insert(node);}
