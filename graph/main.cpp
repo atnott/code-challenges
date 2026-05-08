7aebe2e938200ba9602ab8d7816cee621bf3e4f2
anton krotov <twinkidr@gmail.com>
2026-05-08 16:40:13 +0800

    add class graph

--- graph/main.cpp
+++ graph/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <vector>
 #include <string>
 #include <set>
 using namespace std;
@@ -18,4 +20,26 @@ public:
     node_iterator nb_end() const {return neighbors.end();}
 
     friend class Graph;
+};
+
+class Graph {
+    set<Node*> nodes;
+public:
+    void add_node(Node* node) {nodes.insert(node);}
+
+    void add_edge(Node* node1, Node* node2) {
+        if (nodes.find(node1) == nodes.end() || nodes.find(node2) == nodes.end()) return;
+        node1->add_neighbor(node2);
+        node2->add_neighbor(node1);
+    }
+
+    void remove_node(Node* node) {
+        nodes.erase(node);
+        for (Node* nd : nodes) nd->remove_neighbor(node);
+    }
+
+    using node_iterator = set<Node*>::const_iterator;
+    node_iterator begin() const {return nodes.begin();}
+    node_iterator end() const {return nodes.end();}
+
 };
