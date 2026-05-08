265bc0be700374936ec371276b1ac981ce2d8083
anton krotov <twinkidr@gmail.com>
2026-05-08 22:15:50 +0800

    add first test

--- graph/main.cpp
+++ graph/main.cpp
@@ -3,5 +3,11 @@
 using namespace std;
 
 int main() {
+    Graph graph("testgraph.txt");
 
+    int cnt_node = 0;
+    for (auto it = graph.begin(); it != graph.end(); ++it) {
+        cnt_node++;
+    }
+    cout << cnt_node << endl;
 }
